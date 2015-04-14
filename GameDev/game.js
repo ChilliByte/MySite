62a6ad07525bf0f5644a5f47a4458fd875391953
Deep Sohelia <soheliadeep@gmail.com>
2015-04-14 21:12:06 +0100

    Update game.js

--- GameDev/game.js
+++ GameDev/game.js
@@ -3,7 +3,7 @@
     window.requestAnimationFrame = requestAnimationFrame;
 })();
 
-document.getElementById("char1SpriteSheet").onload = function () {
+function loadSprite() {
     console.log("Loaded");
     var char1Sheet = document.getElementById("char1SpriteSheet");
     console.log("Stored Image");
