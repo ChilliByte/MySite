382ca0b76e8899152e0435943f306538ab9c2325
Deep Sohelia <soheliadeep@gmail.com>
2015-04-17 13:39:52 +0100

    Update game.js

--- GameDev/game.js
+++ GameDev/game.js
@@ -207,8 +207,12 @@ function drawChar() {
             ctx.drawImage(char1Sheet,48,0,16,32,player.x,player.y,units,2*units)
         }
         
-        if ((player.velX == 0) && (player.lastDir == "l")) {console.log("Vel 0, lastDir l")}
-        if ((player.velX == 0) && (player.lastDir == "r")) {console.log("Vel 0, lastDir r")}
+        if ((player.velX == 0) && (player.lastDir == "l")) {
+            ctx.drawImage(char1Sheet,48,0,16,32,player.x,player.y,units,2*units)
+        }
+        if ((player.velX == 0) && (player.lastDir == "r")) {
+            ctx.drawImage(char1Sheet,32,0,16,32,player.x,player.y,units,2*units)
+        }
         
     }
     if (player.char == 2) {
