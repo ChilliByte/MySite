3d8f0d29c54c9cb91fc817e966b3547f6b93ef15
Deep Sohelia <soheliadeep@gmail.com>
2015-04-30 20:12:18 +0100

    Update game.js

--- GameDev/game.js
+++ GameDev/game.js
@@ -23,6 +23,7 @@ function update() {
                 if ((player.x > 3*units) && (player.x < 8*units) && (pathIncrement > 0)) {
                     for (var boxMovementLoop = 0; boxMovementLoop < currentLevel.boxes.length; boxMovementLoop++) {
                         currentLevel.boxes[boxMovementLoop].x+= 2
+                        pathIncrement += 2
                     } 
                 } else {
                     player.velX -= units/8;
@@ -53,9 +54,10 @@ function update() {
         if (currentLevel.type === "path") {
             console.log("Right, Path")
             if (player.velX < player.speed) {
-                if ((player.x > 12*units) && (player.x < 17*units) && (pathIncrement < currentLevel.width - (40*units- player.x))) {
+                if ((player.x > 12*units) && (player.x < 17*units) && (pathIncrement < currentLevel.width*units - (40*units- player.x))) {
                     for (var boxMovementLoop = 0; boxMovementLoop < currentLevel.boxes.length; boxMovementLoop++) {
                         currentLevel.boxes[boxMovementLoop].x-= 2
+                        pathIncrement-=2
                     } 
                 } else {
                     player.velX += units/8;
