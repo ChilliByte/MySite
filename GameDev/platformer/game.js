ab0f0cef0733c6ffae95acf49f9a683d53d20e53
Deep Sohelia <soheliadeep@gmail.com>
2015-05-24 15:21:03 +0100

    Update game.js

--- GameDev/platformer/game.js
+++ GameDev/platformer/game.js
@@ -96,14 +96,14 @@ function animate(x) {
     frame = 0
     animations = [
         {
-            frames: [0,0.05,0.1,0.15,0.2,0.3,0.5,0.7,0.8,0.85,0.9,0.95,1]
+            frames: [0,0.05,0.1,0.15,0.2,0.3,0.5,0.7,0.8,0.85,0.9,0.95,1],
             draw: function () {
                 ctx.fillStyle = "#000"
                 ctx.globalAlpha = animations[x].frames[frame];
                 ctx.beginPath();
                 ctx.fillRect(0,0,40*units,40*units);
                 ctx.closePath();
-                ctx.globalAlpha = 1
+                ctx.globalAlpha = 1;
             }
         }
     ]
