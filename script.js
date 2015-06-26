8094a289469dd9fef47299de090be6f0e53ba259
Deep Sohelia <soheliadeep@gmail.com>
2015-06-26 16:06:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var times = Math.floor((Math.random() * 1000) + 50);
-canvas.height = 1000;
-canvas.width = 500;
+var times = Math.floor((Math.random() * 50) + 10);
+canvas.height = 500;
+canvas.width = 1000;
 while(times--) {
   ctx.fillStyle = "white";
   ctx.fillRect(Math.floor((Math.random() * 1000) + 1),Math.floor((Math.random() * 1000) + 1),5,5);
