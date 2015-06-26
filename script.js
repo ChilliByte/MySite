457dd1a1de6d522507ced95eab9813b0bfdced25
Deep Sohelia <soheliadeep@gmail.com>
2015-06-26 16:09:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,13 @@ canvas.height = 500;
 canvas.width = 1000;
 while(times--) {
   ctx.fillStyle = "white";
-  ctx.fillRect(Math.floor((Math.random() * 1000) + 1),Math.floor((Math.random() * 1000) + 1),5,5);
+  starX = Math.floor((Math.random() * 1000) + 1);
+  starY = Math.floor((Math.random() * 1000) + 1);
+  ctx.fillRect(starX,starY,3,3);
+  
+  ctx.fillRect(starX,starY - 12,3,9);
+  ctx.fillRect(starX,starY + 12,3,9);
+  ctx.fillRect(starX - 12,starY,9,3);
+  ctx.fillRect(starX + 12,starY,9,3);
+  
 }
