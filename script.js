f350f03272b0147e0ba98eee10a64ee6fe948e28
Deep Sohelia <soheliadeep@gmail.com>
2015-06-26 15:57:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("hi")
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var times = Math.floor((Math.random() * 100) + 1);
+while(times--) {
+  ctx.rect(Math.floor((Math.random() * 100) + 1),Math.floor((Math.random() * 100) + 1),5,5);
+  ctx.stroke();
+}
