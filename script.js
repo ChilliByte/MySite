8fda3c44e8b6f258629bcfd6bedce1d447b7705b
Deep Sohelia <soheliadeep@gmail.com>
2015-06-27 00:22:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onload = function() {
     console.log("Loaded")
     document.body.addEventListener('scroll', function () {
+        console.log("scrolling")
         document.body.classList[
             document.body.scrollTop > 100 ? 'add': 'remove'
         ]('scrolled');
