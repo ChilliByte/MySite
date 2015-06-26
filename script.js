f0760d8b79fb2dcafb697390d7b9b4b0cdbb1b2a
Deep Sohelia <soheliadeep@gmail.com>
2015-06-27 00:05:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+window.onload = function() {
+    console.log("Loaded")
+    document.body.addEventListener('scroll', function () {
+        document.body.classList[
+            document.body.scrollTop > window.innerHeight/3 ? 'add': 'remove'
+        ]('scrolled');
+    });
+}
