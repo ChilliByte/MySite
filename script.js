67f6d16ad5b6d3fef71ea7b26d1373a0d762da04
Deep Sohelia <soheliadeep@gmail.com>
2015-06-26 21:51:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,12 +19,12 @@ while(times--) {
 function terrain(x,type) {
     var points = [],
     i = canvas.width;
-    offset = window.innerHeight/4 + Math.floor((Math.random() * 60) + 30);
+    offset = window.innerHeight/4 + Math.floor((Math.random() * 50) + 10);
     while(i--) {
       if (type == "sin") {
-          points.push(4*x/5 + (Math.sin(i/150) * offset));
+          points.push(4*x/5 + (Math.sin(i/175) * offset));
       } else {
-          points.push(4*x/5 + (Math.cos(i/150) * offset));
+          points.push(4*x/5 + (Math.cos(i/175) * offset));
       }
     }
     return points;
@@ -86,13 +86,13 @@ function render() {
   //TERRAIN
   ctx.fillStyle = "#333355";
   for (var t = 1; t < bgTerPoints.length; t++) {
-      ctx.fillRect(t,bgTerPoints[t] + 50,1,1000)
+      ctx.fillRect(t,bgTerPoints[t],1,1000)
   }
 
   ctx.fillStyle = "black";
   ctx.moveTo(0, foreTerPoints[0]);
   for (var t = 1; t < foreTerPoints.length; t++) {
-          ctx.fillRect(t,foreTerPoints[t] + 50,1,1000)
+          ctx.fillRect(t,foreTerPoints[t],1,1000)
   }
 }
 
