a8081cd0839107d8ebb082794db2ccbb879007d1
Deep Sohelia <soheliadeep@gmail.com>
2015-06-26 20:56:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,35 +15,18 @@ while(times--) {
   stars.push([starX,starY,Math.floor((Math.random() * 100) + 1)]);
   
 }
-//TERAIN GENERATION FUNCTION
-//FROM somethinghitme
-//URL: http://www.somethinghitme.com/2013/11/11/simple-2d-terrain-with-midpoint-displacement/
-function terrain(width, height, displace, roughness) {
+
+function terrain() {
     var points = [],
-        // Gives us a power of 2 based on our width
-        power = Math.pow(2, Math.ceil(Math.log(width) / (Math.log(2))));
-
-    // Set the initial left point
-    points[0] = height / 2 + (Math.random() * displace * 2) - displace;
-    // set the initial right point
-    points[power] = height / 2 + (Math.random() * displace * 2) - displace;
-    displace *= roughness;
-
-    // Increase the number of segments
-    for (var i = 1; i < power; i *= 2) {
-        // Iterate through each segment calculating the center point
-        for (var j = (power / i) / 2; j < power; j += power / i) {
-            points[j] = ((points[j - (power / i) / 2] + points[j + (power / i) / 2]) / 2);
-            points[j] += (Math.random() * displace * 2) - displace
-        }
-        // reduce our random range
-        displace *= roughness;
+    i = canvas.width;
+    while(i--) {
+      points.push(i);
     }
     return points;
 }
 
-var foreTerPoints = terrain(canvas.width * 10, canvas.height, canvas.height / 3, 0.5);
-var bgTerPoints = terrain(canvas.width * 10, canvas.height, canvas.height / 3, 0.5);
+var foreTerPoints = terrain();
+var bgTerPoints = terrain();
 
 //MOON
 var moonX = Math.floor((Math.random() * window.innerWidth - 250) + 250);
