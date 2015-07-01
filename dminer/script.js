b18d3ee975064f0ce89ad53a51539eaf6366dffd
Deep Sohelia <soheliadeep@gmail.com>
2015-07-01 11:49:10 +0100

    Update script.js

--- dminer/script.js
+++ dminer/script.js
@@ -1,5 +1,9 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var tilesX = 16;
+var tilesY = 9;
+canvas.height = Math.ceil(window.innerHeight/tilesX)*tilesX;
+canvas.width = Math.ceil(window.innerWidth/tilesY)*tilesY;
 // shim layer with setTimeout fallback
 window.requestAnimFrame = (function(){
   return  window.requestAnimationFrame       ||
@@ -17,7 +21,7 @@ var x = 1;
 ctx.fillStyle = "#00ff00";
 function render() {
   ctx.clearRect(0,0,1000,1000);
-  ctx.fillRect(x/100,0,10,10);
+  ctx.fillRect(x/10,0,10,10);
   x++;
 }
 
