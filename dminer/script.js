8bcbf0c52a90e35f69584edd429bf383825c5b21
Deep Sohelia <soheliadeep@gmail.com>
2015-07-01 11:44:30 +0100

    Update script.js

--- dminer/script.js
+++ dminer/script.js
@@ -1,1 +1,29 @@
-alert("hello world");
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
+
+// usage:
+// instead of setInterval(render, 16) ....
+var x = 1;
+ctx.fillStyle = "#00ff00";
+function render() {
+  ctx.clearRect(0,0,1000,1000);
+  ctx.drawRect(x/100,0,10,10);
+  x++;
+}
+
+(function animloop(){
+  requestAnimFrame(animloop);
+  render();
+})();
+// place the rAF *before* the render() to assure as close to
+// 60fps with the setTimeout fallback.
