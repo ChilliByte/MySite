0d795796d3cb18ac999209256b643008c357c3c4
Deep Sohelia <soheliadeep@gmail.com>
2015-07-01 15:45:34 +0100

    Update script.js

--- dminer/script.js
+++ dminer/script.js
@@ -15,13 +15,23 @@ var tilesY = 8;
 var tileWidth = Math.ceil(window.innerWidth/tilesX)
 document.getElementsByTagName("svg")[0].height = tileWidth * tilesY;
 document.getElementsByTagName("svg")[0].width  = tileWidth * tilesX;
+var xPos = 0;
+var yPos = 0;
+var counter = 0;
 var svgns = "http://www.w3.org/2000/svg";
 for (var x = 0; x < 100; x++) {
     var rect = document.createElementNS(svgns, 'rect');
     rect.setAttributeNS(null, 'x', xPos);
     rect.setAttributeNS(null, 'y', yPos);     
-    rect.setAttributeNS(null, 'height', '50');
-    rect.setAttributeNS(null, 'width', '50');
+    rect.setAttributeNS(null, 'height', ''+tileWidth);
+    rect.setAttributeNS(null, 'width', ''+tileWidth);
     rect.setAttributeNS(null, 'fill', getRandomRed());
     document.getElementById('svgOne').appendChild(rect);
+    xPos += tileWidth;
+    counter++
+    if (counter > tilesX) {
+      xPos = 0;
+      counter = 0;
+      yPos+=tileWidth;
+    }
 }
