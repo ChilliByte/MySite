97a092b5d884124673e10bc652cb6fc8937a91b7
Deep Sohelia <soheliadeep@gmail.com>
2015-07-30 00:23:43 +0100

    Update script.js

--- templates/template2/script.js
+++ templates/template2/script.js
@@ -5,7 +5,7 @@ window.onload = function() {
         console.log(evt.target.tagName)
         if((evt.target.tagName == "A") && (evt.target.innerText == "Read more")) {
             evt.preventDefault();
-            evt.target.parentNode.style.background = "#OOFFOO";
+            evt.target.parentNode.setAttribute("style","#OOFFOO");
         }
     }, false);
 }
