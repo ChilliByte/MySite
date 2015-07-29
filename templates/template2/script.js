0e6ff5dc7f3f5f60f3f57fa8990edf8804031c1e
Deep Sohelia <soheliadeep@gmail.com>
2015-07-29 23:52:32 +0100

    Update script.js

--- templates/template2/script.js
+++ templates/template2/script.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     document.addEventListener('click', function(evt) {
         console.log(evt.target.parentNode);
-        if(evt.target.tagName == "a" && evt.tag.innerText = "Read More") {
+        if(evt.target.tagName == "a" && evt.tag.innerText == "Read More") {
             alert("YAY!")
         }
     }, false);
