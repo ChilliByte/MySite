20ee8807edce0ebd02c80a8d8441b96cce5c7c40
Deep Sohelia <soheliadeep@gmail.com>
2015-07-30 01:10:28 +0100

    Update script.js

--- templates/template2/script.js
+++ templates/template2/script.js
@@ -7,10 +7,10 @@ window.onload = function() {
             evt.preventDefault();
             evt.target.parentNode.className = "fs";
             evt.target.innerText = "Go back"
-        }
-        if((evt.target.tagName == "A") && (evt.target.innerText == "Go back")) {
+        } else if((evt.target.tagName == "A") && (evt.target.innerText == "Go back")) {
             evt.preventDefault();
             evt.target.parentNode.className = "";
+            evt.target.innerText = "Read more";
         }
     }, false);
 }
