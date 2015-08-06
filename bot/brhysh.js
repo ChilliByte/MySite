0544bc341d068dbd196734e4bdca1a8a0b3fba04
Deep Sohelia <soheliadeep@gmail.com>
2015-08-06 21:08:47 +0100

    Update brhysh.js

--- bot/brhysh.js
+++ bot/brhysh.js
@@ -1,1 +1,9 @@
 console.log("Lumberbot loaded");
+console.log("Running Bot code");
+document.getElementById("live-comments-input-field").addEventListener("keydown", function(e){
+  console.log("Keydown event fired");
+  console.log(e);
+  console.log(e.keyCode);
+  console.log(e.key);
+  console.log(e.char);
+});
