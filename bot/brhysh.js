8e07476c51c3877eb4984e9012f2a7feb1b0fc09
Deep Sohelia <soheliadeep@gmail.com>
2015-08-06 21:26:20 +0100

    Update brhysh.js

--- bot/brhysh.js
+++ bot/brhysh.js
@@ -2,13 +2,13 @@ console.log("Lumberbot loaded");
 console.log("Running Bot code");
 var listening = false;
 document.getElementById("live-comments-input-field").addEventListener("keydown", function(e){
-  console.log("Keydown event fired");
-  console.log(e.keyCode);
   if(e.keyCode == 223) {
     console.log("command started");
     listening = true;
   }
   if(e.keyCode == 13) {
+    console.log("Input: " + document.getElementById("live-comments-input-field").innerText)
+    console.log("Hidden: " + document.getElementById("live-comments-input-field-hid").value)
     alert();
     console.log("Message sent")
     listening = false;
