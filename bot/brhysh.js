6541beef5231255ceba559377189214e6e8f3890
Deep Sohelia <soheliadeep@gmail.com>
2015-08-06 21:16:06 +0100

    Update brhysh.js

--- bot/brhysh.js
+++ bot/brhysh.js
@@ -8,7 +8,7 @@ document.getElementById("live-comments-input-field").addEventListener("keydown",
     console.log("command started");
     listening = true;
   }
-  if(e.keyCode = 13) {
+  if(e.keyCode == 13) {
     console.log("Message sent")
     listening = false;
   }
