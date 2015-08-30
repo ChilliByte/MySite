ab10e9cc3102772d27d8016a0c4a42744bc301cc
Deep Sohelia <soheliadeep@gmail.com>
2015-08-30 19:47:19 +0100

    Update game.js

--- GameDev/platformer/game.js
+++ GameDev/platformer/game.js
@@ -394,6 +394,7 @@ function checkPlayerDoorCollision() {
             player.velX = 0;
             player.jumping = false;
             scrolling = false;
+            if (dir == "l") {player.x+=10}else{player.x-=10}
         } else if (dir === "b") {
             touchingIce = false;
             if (gravityDown) {
@@ -452,6 +453,7 @@ function checkPlayerBoxCollision() {
         player.velX = 0;
         player.jumping = false;
         scrolling = false;
+        if (dir == "l") {player.x+=10}else{player.x-=10}
         if(currentLevel.boxes[i].waterEdge) {touchingEdge = true}
     } else if (dir === "b") {
         touchingIce = false;
