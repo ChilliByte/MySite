2ea2c005711b5011a8cd018e53be5f7378254783
Deep Sohelia <soheliadeep@gmail.com>
2015-12-28 17:14:22 +0000

    Update game.js

--- GameDev/platformer/game.js
+++ GameDev/platformer/game.js
@@ -44,17 +44,15 @@ function update() {
     if ((player.lastDir == "l") && (currentLevel.offset > currentLevel.width - (tilesX*units))) {
         scrolling = false;
     }
-    if(collided) {
-        console.log("Collided");
-    }
+
     if (!scrolling) {
         player.x += player.velX;
         if ((player.x < 9*units) && (currentLevel.offset > 0)) {
-            player.x = 9.01*units;
+            player.x = 9.5*units;
             console.log("Pushing Forward");
         }
         if ((player.x > 10*units) && (currentLevel.offset < currentLevel.width - (tilesX*units))) {
-            player.x = 9.99*units;
+            player.x = 9.5*units;
             console.log("Pushing Backward");
         }
     } else if(!collided) {
