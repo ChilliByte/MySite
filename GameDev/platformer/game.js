544f895133b491d1ddf9012e5bdd9faea6e498e7
Deep Sohelia <soheliadeep@gmail.com>
2015-12-30 21:49:48 +0000

    Update game.js

--- GameDev/platformer/game.js
+++ GameDev/platformer/game.js
@@ -727,6 +727,60 @@ function drawMobs() {
     ctx.fill();
 }
 
+function checkPlayerCrateCollision() {
+    var dir = colCheck(player, currentLevel.crates[i],true);
+    //Do something depending on the direction the collision happened from.
+    if (dir === "l" || dir === "r") {
+        currentLevel.crates[i].velX = player.velX*0.75;
+        player.velX *=0.75;
+        player.jumping = false;
+    } else if (dir === "b") {
+        touchingIce = false;
+        if (gravityDown) {
+            player.grounded = true;
+            player.jumping = false;
+        } else {    
+            currentLevel.crates[i].broken = true;
+        }
+    } else if (dir === "t") {
+        touchingIce = false;
+        if (gravityDown) {
+            currentLevel.crates[i].broken = true;
+        } else {
+            player.grounded = true;
+            player.jumping = false;
+        }
+    } else {
+        touchingEdge = false;
+    }
+}
+
+function checkPlayerCrateCollision() {
+    j = currentLevel.boxes.length;
+    while(j--) {
+        var dir = colCheck(currentLevel.crates[i],currentLevel.boxes[j],true);
+        //Do something depending on the direction the collision happened from.
+        if (dir === "l" || dir === "r") {
+            currentLevel.crates[i].velX = 0;
+            player.velX *= 0;
+            currentLevel.crates[i].grounded = true;
+        } else if (dir === "b") {
+            if (gravityDown) {
+                currentLevel.crates[i].grounded = true;
+            } else {    
+                currentLevel.crates[i].broken = true;
+            }
+        } else if (dir === "t") {
+            if (gravityDown) {
+                currentLevel.crates[i].broken = true;
+            } else {
+                currentLEvel.crates[i].grounded = true;
+            }
+        }
+    }
+}
+
+
 function drawCrates() {
     ctx.beginPath();
     ctx.fillStyle = "#FF6";
