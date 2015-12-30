ea802cdb36dff86f5251e22f7af27f61a2ec8734
Deep Sohelia <soheliadeep@gmail.com>
2015-12-30 20:27:02 +0000

    Update game.js

--- GameDev/platformer/game.js
+++ GameDev/platformer/game.js
@@ -616,7 +616,7 @@ function drawIce() {
 function moveProjectiles() {
     currentLevel.projectiles[i].x += currentLevel.projectiles[i].velX;
     currentLevel.projectiles[i].y -= currentLevel.projectiles[i].velY;
-    currentLevel.projectiles[i].velY -= gravity;
+    currentLevel.projectiles[i].velY -= gravity/5;
     currentLevel.projectiles[i].velX = Math.sqrt((currentLevel.projectiles[i].speed * currentLevel.projectiles[i].speed) - (currentLevel.projectiles[i].velY * currentLevel.projectiles[i].velY))
 }
 
