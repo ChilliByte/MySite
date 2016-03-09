a50ba61776bba5b554fe92ca47f98b279fdbce8a
Deep Sohelia <soheliadeep@gmail.com>
2016-03-09 14:53:53 +0000

    Update script.js

--- graphs/script.js
+++ graphs/script.js
@@ -32,8 +32,8 @@ nodes.push(new Person("Sam W","Sophie",true));
 nodes.push(new Person("Steven","Vani",true));
 nodes.push(new Person("Vani","Mandeep",false));
 nodes.push(new Person("Mandeep","Daisy",false));
-nodes.push(new Person("Daisy","Hannah",false));
-/*nodes.push(new Person("Hannah","Mahesh",false));
+/*nodes.push(new Person("Daisy","Hannah",false));
+nodes.push(new Person("Hannah","Mahesh",false));
 nodes.push(new Person("Mahesh","Aba",true));
 nodes.push(new Person("Aba","Dom",false));
 nodes.push(new Person("Dom","Benji",true));
@@ -90,8 +90,8 @@ switch(graphShape) {
 					} else {
 						ctx.fillStyle="#ff0055"
 					}
-					x = (0.75+(0.5*Math.random()))*(i+1)*(w/(gridSize+1));
-					y = (0.75+(0.5*Math.random()))*(j+1)*(h/(gridSize+1))
+					x = 0.9*(0.75+(0.5*Math.random()))*(i+1)*(w/(gridSize+1));
+					y = 0.9*(0.75+(0.5*Math.random()))*(j+1)*(h/(gridSize+1))
 					ctx.arc(x,y,10,0,2*Math.PI);
 					ctx.fill();
 					ctx.font = "12px Calibri";
@@ -100,6 +100,12 @@ switch(graphShape) {
 				}
 			}
 		}
+		break;
+	case "Triangular" :
+		x1=-1/2+Math.pow(1 + (-4*(2*numNodes)),0.5)/2;
+		x2=-1/2-Math.pow(1 + (-4*(2*numNodes)),0.5)/2;
+		var rows = Math.max(x1,x2);
+		console.log(rows);
 		break;
 }
 
