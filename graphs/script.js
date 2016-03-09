cb40eb60f5e92adf1803a6b4d58cde49db8ce4f9
Deep Sohelia <soheliadeep@gmail.com>
2016-03-09 13:14:17 +0000

    Update script.js

--- graphs/script.js
+++ graphs/script.js
@@ -1,3 +1,12 @@
+function person(name,to,gen) {
+	this.name = name;
+	this.to = to;
+	this.gender = gen;
+}
+
+var nodes = [];
+
+
 var canvas = document.getElementById("graph");
 var ctx = canvas.getContext("2d");
 var h = window.innerHeight;
@@ -6,4 +15,6 @@ canvas.height = h;
 canvas.width = w;
 
 ctx.font = "30px Arial";
-ctx.fillText("Hello World",50,w/2);
+ctx.fillText("Graphs",50,w/2);
+
+
