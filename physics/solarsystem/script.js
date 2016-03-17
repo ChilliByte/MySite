c99232346df1043469f750a7f4214de80eeb8a84
Deep Sohelia <soheliadeep@gmail.com>
2016-03-17 13:49:10 +0000

    Update script.js

--- physics/solarsystem/script.js
+++ physics/solarsystem/script.js
@@ -10,6 +10,7 @@ var origin = {
 	x: w/2,
 	y: h/2
 }
+var testAngMom = false;
 //Gravitational Constant, increase to strengthen gravity
 var bigG = 0.025;
 
@@ -84,6 +85,11 @@ function getNetPull(p1) {
 			//adds up all of the horizonal and vertical forces acting on the particle
 		}
 	}
+	if(testAngMom){
+	angMom = getComponentVectors(getBearing(origin,p1),0.001 * Math.sqrt(getDistanceSquared(origin,p1)));
+	p1.cX += angMom.x;
+	p1.cY += angMom.y;
+	}	
 }
 
 var i = particleCount;
