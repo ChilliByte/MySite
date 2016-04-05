f3f1b97eb0c05bdde3a5fcd8ac20c16404db55bc
Deep Sohelia <soheliadeep@gmail.com>
2016-04-05 15:44:20 +0100

    Update bean.js

--- GameDev/smolbean/bean.js
+++ GameDev/smolbean/bean.js
@@ -57,7 +57,7 @@ function drawBean(bean) {
           ' </g>'+
           ' <g>'+
           '  <title>Layer 1</title>'+
-          '  <path fill="'+bean.color+" fill-opacity="null" stroke="#000" stroke-width="1.5" stroke-linejoin="null" stroke-linecap="null" stroke-opacity="null" d="m9,92c-8,-46.72376 1.0663,-91 53,-91c51.9337,0 19,83.13812 61,82c42,-1.13812 21.98343,61.61188 11.47514,73.14641c-10.50829,11.53453 -31.50829,12.85359 -57.47514,12.85359c-51.9337,0 -60,-30.27624 -68,-77z" id="svg_1"/>'+
+          '  <path fill="'+bean.color+'" fill-opacity="null" stroke="#000" stroke-width="1.5" stroke-linejoin="null" stroke-linecap="null" stroke-opacity="null" d="m9,92c-8,-46.72376 1.0663,-91 53,-91c51.9337,0 19,83.13812 61,82c42,-1.13812 21.98343,61.61188 11.47514,73.14641c-10.50829,11.53453 -31.50829,12.85359 -57.47514,12.85359c-51.9337,0 -60,-30.27624 -68,-77z" id="svg_1"/>'+
           '  <ellipse fill="'+bean.eyecolor+'" stroke="#000" stroke-width="1.5" stroke-opacity="null" fill-opacity="null" cx="46.5" cy="72.5" id="svg_4" rx="7" ry="7"/>'+
           '  <ellipse fill="'+bean.eyecolor+'" stroke-width="1.5" stroke-opacity="null" fill-opacity="null" cx="71.5" cy="68" id="svg_5" rx="7" ry="7" stroke="#000"/>'+
           '  <path stroke="#000" fill="#fff" fill-opacity="null" stroke-width="1.5" stroke-linejoin="null" stroke-linecap="null" stroke-opacity="null" d="m32.159,98.23217c0,-20.96475 18.11972,-10.99889 40.48973,-10.99889c22.37001,0 40.48973,-9.96584 40.48973,10.99889c0,20.96475 -18.11972,37.94619 -40.48973,37.94619c-22.37001,0 -40.48973,-16.98143 -40.48973,-37.94619z" id="svg_7" transform="rotate(171 72.64873504638672,110.52222442626953) "/>'+
@@ -94,7 +94,7 @@ function drawBean(bean) {
           '<g>'+
           '<title>Layer 1</title>'+
           '<g id="svg_2">'+
-          '<path fill="'+ bean.colot+'" fill-opacity="null" stroke="#000" stroke-width="1.5" stroke-linejoin="null" stroke-linecap="null" stroke-opacity="null" d="m9,92c-8,-46.72376 1.0663,-91 53,-91c51.9337,0 19,83.13812 61,82c42,-1.13812 21.98343,61.61188 11.47514,73.14641c-10.50829,11.53453 -31.50829,12.85359 -57.47514,12.85359c-51.9337,0 -60,-30.27624 -68,-77z" id="svg_1"/>'+
+          '<path fill="'+ bean.color+'" fill-opacity="null" stroke="#000" stroke-width="1.5" stroke-linejoin="null" stroke-linecap="null" stroke-opacity="null" d="m9,92c-8,-46.72376 1.0663,-91 53,-91c51.9337,0 19,83.13812 61,82c42,-1.13812 21.98343,61.61188 11.47514,73.14641c-10.50829,11.53453 -31.50829,12.85359 -57.47514,12.85359c-51.9337,0 -60,-30.27624 -68,-77z" id="svg_1"/>'+
           '<ellipse fill="'+bean.eyecolor+'" stroke="#000" stroke-width="1.5" stroke-opacity="null" fill-opacity="null" cx="46.5" cy="72.5" id="svg_4" rx="7" ry="7"/>'+
           '<ellipse fill="'+bean.eyecolor+'" stroke-width="1.5" stroke-opacity="null" fill-opacity="null" cx="71.5" cy="68" id="svg_5" rx="7" ry="7" stroke="#000"/>'+
           '<path fill="#f99" fill-opacity="null" stroke-width="1.5" stroke-linejoin="null" stroke-linecap="null" stroke-opacity="null" d="m50.25,91.98459c0,-8.29581 6.71271,-4.35229 15,-4.35229c8.28729,0 15,-3.94351 15,4.35229c0,8.29581 -6.71271,15.01541 -15,15.01541c-8.28729,0 -15,-6.7196 -15,-15.01541z" id="svg_7" stroke="#000" transform="rotate(-10 65.24999999999974,96.84779357910153) "/>'+
