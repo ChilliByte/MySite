acd47c6cd94f85a734a0b9c37c13f2c2bc5a2705
Deep Sohelia <soheliadeep@gmail.com>
2016-04-05 16:46:33 +0100

    Update main.js

--- GameDev/smolbean/main.js
+++ GameDev/smolbean/main.js
@@ -1,3 +1,17 @@
 function play() {
   document.getElementById("title").style.display = "none";
 }
+function createBean() {
+  player = new Bean(beanColor.value,beaneyecolor.value,beanName.value);
+  document.getElementById("beanSvg").style.display = "none";
+}
+
+window.onload = function() {
+  var beanCols = document.querySelectorAll("input[type=color]");
+  for(var i = 0; i < beanCols.length; i++) {
+    beanCols[i].onchange = function(e) {
+      alert("color changed to " + this.value);
+      console.log(e);
+    }
+  }
+}
