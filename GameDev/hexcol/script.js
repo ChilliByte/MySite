b8ac0c69edc0ccda5c1754852000d3f8496bd372
Deep Sohelia <soheliadeep@gmail.com>
2016-04-05 08:15:19 +0100

    Update script.js

--- GameDev/hexcol/script.js
+++ GameDev/hexcol/script.js
@@ -1,8 +1,48 @@
+var points = 0;
+var currentSection;
+var wrong;
+var shiftedCol;
+var shiftArray = [30,15,5,1];
+var correctCol;
+var count = 0;
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 function getRandomColor() {
-  return "#000000";
+  return "#" + getRandomInt(0,256).toString(16) + "" + getRandomInt(0,256).toString(16) + "" + getRandomInt(0,256).toString(16);
+}
+
+function getWrongColor(correct,diff) {
+  shiftedCol = 1+(2*getRandomInt(0,3));
+  wrongCol = correct.slice(shiftedCol,shiftedCol+2);
+  wrongCol = parseInt(wrongCol,16);
+  if(wrongCol < shiftArray[diff]) {
+    wrongCol += shiftArray[diff];
+  } else {
+    wrongCol -= shiftArray[diff];
+  }
+  wrongCol = wrongCol.toString(16);
+  wrongCol = correct.slice(0,shiftedCol)+wrongCol+correct.slice(shiftedCol+2);
+}
+
+function hideMenu() {
+    document.getElementsByClassName("menu")[0].style.display = "none";
 }
 
 function play(difficulty) {
-  document.getElementsByClassName("menu")[0].style.display = "none";
-  document.querySelectorAll("section.game")[difficulty].style.display = "block";
+  currentSection = document.querySelectorAll("section.game")[difficulty]
+  currentSection.style.display = "block";
+  count = currentSection.children.length
+  wrong = getRandomInt(0,count);
+  correctCol = getRandomColor();
+  wrongCol = getWrongColor(correctCol,difficulty);
+  while(count--) {
+    if(count == wrong) {
+      currentSection.children[count].style.backgroundColor = wrongCol;
+    } else {
+      currentSection.children[count].style.backgroundColor = correctCol;
+    }
+  }
 }
