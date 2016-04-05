f6efa8e03010969cec8302ab3f85dbeaa35b8ff9
Deep Sohelia <soheliadeep@gmail.com>
2016-04-05 10:27:42 +0100

    Create core.js

---
+++ GameDev/smolbean/core.js
@@ -0,0 +1,26 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext("2d");
+var h = window.innerHeight;
+var w = window.innerWidth;
+canvas.height = h;
+canvas.width = w;
+
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
+// usage:
+// instead of setInterval(render, 16) ....
+function render() {
+ 
+}
+(function animloop(){
+  requestAnimFrame(animloop);
+  render();
+})();
