e5a4f38c19eaa4148c75a2855d9935a07670aacd
Deep Sohelia <soheliadeep@gmail.com>
2016-04-05 11:00:52 +0100

    Create bean.js

---
+++ GameDev/smolbean/bean.js
@@ -0,0 +1,14 @@
+function Bean(color,name) {
+  this.x = 10;
+  this.y = 10;
+  this.velX = 0;
+  this.velY = 0;
+  this.name = name;
+  this.color = color;
+  this.fun = 100;
+  this.food = 100;
+  this.water = 100;
+  this.love = 100;
+}
+
+var Deep = new Bean("#07E","Deep");
