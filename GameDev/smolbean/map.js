6d6e4dc988fd5df592809df1c6064a4ce3ae2e2f
Deep Sohelia <soheliadeep@gmail.com>
2016-04-14 10:02:25 +0100

    Update map.js

--- GameDev/smolbean/map.js
+++ GameDev/smolbean/map.js
@@ -4,26 +4,37 @@ function Box(x,y,h,w) {
   this.h = h;
   this.w = w;
 }
+function SpecialBox(type,x,y,h,w) {
+  this.x = x;
+  this.y = y;
+  this.h = h;
+  this.w = w;
+  this.type = type;
+}
 
-var kitchen = {};
-var bedroom = {};
-var livingRoom = {};
-var bathroom = {};
-var hallway = {};
-var landing = {};
-var driveway = {};
+function Room() {
+  this.boxes = [];
+  this.specialBoxes = [];
+}
 
-var mainStreet1 = {};
-var mainStreet2 = {};
-var mainStreet3 = {};
-var sideAlley = {};
+var kitchen = new Room();
+var bedroom = new Room();
+var livingRoom = new Room();
+var bathroom = new Room();
+var hallway = new Room();
+var landing = new Room();
+var driveway = new Room();
 
-var clothesShop = {};
-var doctors = {};
-var arcade = {};
-var restaurant = {};
-var furnitureShop = {};
+var mainStreet1 = new Room();
+var mainStreet2 = new Room();
+var mainStreet3 = new Room();
+var sideAlley = new Room();
 
+var clothesShop = new Room();
+var doctors = new Room();
+var arcade = new Room();
+var restaurant = new Room();
+var furnitureShop = new Room();
   
 var worldMap = [
   [bedroom,landing,bathroom],
