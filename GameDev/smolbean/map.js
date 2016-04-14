4609dd48bc9ae5ddc5e11136b07ee013623e4575
Deep Sohelia <soheliadeep@gmail.com>
2016-04-14 09:27:24 +0100

    Create map.js

---
+++ GameDev/smolbean/map.js
@@ -0,0 +1,23 @@
+var kitchen = {};
+var bedroom = {};
+var livingRoom = {};
+var bathroom = {};
+var hallway = {};
+var landing = {};
+var driveway = {};
+var mainStreet1 = {};
+var mainStreet2 = {};
+var mainStreet3 = {};
+var sideAlley = {};
+
+var clothesShop = {};
+var doctors = {};
+var arcade = {};
+var restaurant = {};
+var furnitureShop = {};
+
+var houseMap = [
+  [bedroom,landing,bathroom],
+  [livingRoom,hallway,kitchen]
+]
+var worldMap = [driveway,mainStreet1,mainStreet2,mainStreet3,sideAlley]
