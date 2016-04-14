4e644d63bc5924fb388162020606edb11de66d5b
Deep Sohelia <soheliadeep@gmail.com>
2016-04-14 15:22:03 +0100

    Update main.js

--- GameDev/smolbean/main.js
+++ GameDev/smolbean/main.js
@@ -151,5 +151,13 @@ function drawArrows() {
         ctx.lineTo(160,490);
         ctx.lineTo(80,450);
         ctx.fill();
+    }    
+    if(currentLevel.right) {
+        ctx.fillStyle = "#fd0"
+        ctx.beginPath();
+        ctx.moveTo(1440,410);
+        ctx.lineTo(1440,490);
+        ctx.lineTo(1520,450);
+        ctx.fill();
     }
 }
