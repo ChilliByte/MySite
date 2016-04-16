d864661f32b1d1a1376d41a03d410d4ab519ad42
Deep Sohelia <soheliadeep@gmail.com>
2016-04-16 09:03:47 +0100

    Update map.js

--- GameDev/smolbean/map.js
+++ GameDev/smolbean/map.js
@@ -14,7 +14,9 @@ function SpecialBox(type,x,y,h,w) {
 
 function Room(u,d,l,r) {
   this.boxes = [
-    new Box(0,850,50,1600)
+    new Box(0,850,50,1600),
+    new Box(-30,0,900,30),    
+    new Box(1600,0,900,30),
   ];
   this.above = u;
   this.below = d;
@@ -29,7 +31,7 @@ var livingRoom    = new Room(false, false, false, true);
 var bathroom      = new Room(false, false, true, false);
 var hallway       = new Room(true,  false, true,  true);
 var landing       = new Room(false, true , true,  true);
-var driveway      = new Room(false, false, true,  true);
+var driveway      = new Room(false, false, false, true);
 
 var mainStreet1   = new Room(false, false, true,  true);
 var mainStreet2   = new Room(false, false, true,  true);
@@ -45,5 +47,5 @@ var furnitureShop = new Room(false, true, false,  false);
 var worldMap = [
   [bedroom,landing,bathroom],
   [livingRoom,hallway,kitchen],
-  [driveway,mainStreet1,mainStreet2,mainStreet3,sideAlley]
+  [null,driveway,mainStreet1,mainStreet2,mainStreet3,sideAlley]
 ]
