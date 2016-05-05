734601aae2e14b4c098dedece3da3b72f3b23f4a
Deep Sohelia <soheliadeep@gmail.com>
2016-05-05 08:19:41 +0100

    Update levels.js

--- GameDev/platformer/levels.js
+++ GameDev/platformer/levels.js
@@ -337,7 +337,7 @@ level3.boxes.push(
     new Box(100,12,1,4),
     new Box(110,11,1,3),
     new Box(120,8,1,2),
-    new Box(130,8,1,1),
+    new Box(128,8,1,1),
     new Box(134,16,4,2),
     
     //Ending Sequence
