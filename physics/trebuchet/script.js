b9af3d2642d9d46e4a87027b7c78391dd06bca11
Deep Sohelia <soheliadeep@gmail.com>
2018-10-03 02:15:39 +0100

    Update script.js

--- physics/trebuchet/script.js
+++ physics/trebuchet/script.js
@@ -23,8 +23,8 @@ var render = Render.create({
 });
 
 // create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
+var boxA = Bodies.rectangle(50, 598, 300, 12);
+var boxB = Bodies.rectangle(200, 400, 12, 198);
 var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
 // add all of the bodies to the world
