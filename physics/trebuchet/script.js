583bd60a864f37926167ccc9b039400df06b0573
Deep Sohelia <soheliadeep@gmail.com>
2018-10-09 12:07:31 +0100

    Update script.js

--- physics/trebuchet/script.js
+++ physics/trebuchet/script.js
@@ -1,5 +1,40 @@
+var canvas,ctx,sb, au, aa;
+var config = {
+    valid:false,
+    reason:"",
+    angle:0,
+    distance:0,
+    permExt:false
+};
+
 window.onload = function() {
     document.getElementById("run").addEventListener("click", function(){
-        alert("bingo");
+        if(validInput()) {
+            /*checkValidConfig();
+            findAngle();
+            findDistance();
+            drawConfig();
+            createDataString();
+            output(data);*/alert("valid")
+        else {
+            output("Please make sure the inputs are filled with valid numbers. The first two should be integers from 0-19 inclusive, and the last one 0-8 inclusive.");   
+        }
     });
+    canvas = document.getElementById("canv");
+    ctx = canvas.getContext("2d");
+}
+
+function validInput() {
+    sb = document.getelementsByTagName("input")[0].value;
+    au = document.getelementsByTagName("input")[1].value;
+    aa = document.getelementsByTagName("input")[2].value;
+    if(sb !== "" && au !== "" && aa !== "" && sb >=0 && au >= 0 && aa >= 0 && sb < 20 && au < 20 && aa < 9) {
+        return true
+    } else {
+        return false
+    }
+}
+    
+function output(str) {
+    document.getElementById("output").innerHTML = str;    
 }
