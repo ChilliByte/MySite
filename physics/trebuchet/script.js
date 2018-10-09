efee2a1c27cf27761b3045c69d97d9c35696d779
Deep Sohelia <soheliadeep@gmail.com>
2018-10-09 12:33:28 +0100

    Update script.js

--- physics/trebuchet/script.js
+++ physics/trebuchet/script.js
@@ -1,4 +1,4 @@
-var canvas,ctx,sb, au, aa;
+var canvas,ctx,sb, au, aa,uL,aL,sE;
 var config = {
     valid:false,
     reason:"",
@@ -6,12 +6,13 @@ var config = {
     distance:0,
     permExt:false
 };
+var springRestLength = 97/*mm*/
 
 window.onload = function() {
     document.getElementById("run").addEventListener("click", function(){
         if(validInput() && validConfig()) {
-            /*findAngle();
-            findDistance();
+            findAngle();
+            /*findDistance();
             drawConfig();
             createDataString();
             output(data);*/
@@ -22,6 +23,11 @@ window.onload = function() {
     });
     canvas = document.getElementById("canv");
     ctx = canvas.getContext("2d");
+    canvas.width = 500;
+    canvas.height = 500;
+    ctx.rect(480, 480, 300, 12);
+    ctx.rect(630, 282, 12, 210);
+    ctx.fill()
 }
 
 function validInput() {
@@ -37,6 +43,26 @@ function validInput() {
 function validConfig() {
     if(sb < au) { return true;}
     return false;
+}
+
+function findAngle() {
+    uL = au-sb*10;
+    aL = 50 + 10*aa;
+    sE = springRestLength;
+    if((uL - aL) > springRestLength) {
+        config.permExt = true;    
+        sE = uL - aL;
+    }
+    theta = (Math.PI/2) - Math.acos(((aL*aL)+(uL*uL)-(sE*sE))/(2*aL*uL));
+    output(theta);
+    /*
+    cosine rule a2 = b2 + c2 - 2bccosA
+    a = sE
+    b = aL
+    c = uL
+    A = theta
+    cos A = b2 +c2 - a2 all over 2bc
+    */
 }
 
 function output(str) {
