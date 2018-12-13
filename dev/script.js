58cb9ad62fffa398efa683f76a77b74fd944639d
Deep Sohelia <soheliadeep@gmail.com>
2018-12-13 00:52:41 +0000

    adding scroll detection

--- dev/script.js
+++ dev/script.js
@@ -1,6 +1,6 @@
 var ynought,ymax;
 window.onload = function () {
-	console.log("test");
+	window.scrollTo(0,0);
 	ynought = document.querySelector("#icons a:nth-of-type(1)").getBoundingClientRect().y;
 	ymax = document.body.offsetHeight - (document.documentElement.clientWidth/20)
 	document.getElementById("scroller").style.top = parseInt(ynought - (document.documentElement.clientWidth/20)) + "px";
@@ -8,7 +8,6 @@ window.onload = function () {
 }
 
 window.addEventListener('scroll', function(e) {
-	console.log("x")
 	document.getElementById("scroller").style.top = parseInt(ynought - (document.documentElement.clientWidth/20) + ((ymax-ynought + (document.documentElement.clientWidth/20)) * window.scrollY /(document.body.scrollHeight - document.body.offsetHeight))) + "px";
 	if(parseInt(document.getElementById("scroller").style.top) < ynought) { 
 		document.getElementById("scroller").style.height = (document.documentElement.clientWidth/20) + parseInt(document.getElementById("scroller").style.top) - ynought + "px";
@@ -16,4 +15,15 @@ window.addEventListener('scroll', function(e) {
 	} else {
 		document.getElementById("scroller").style.height = parseInt(document.documentElement.clientWidth/20) + "px"
 	} 
+	
+	if(window.scrollY > document.getElementsByTagName("section")[0].offsetHeight + document.getElementsByTagName("section")[1].offsetHeight + document.getElementsByTagName("section")[2].offsetHeight + document.getElementsByTagName("section")[3].offsetHeight/2) {
+			document.getElementsByTagName("section")[4].className = "active";
+	} else if(window.scrollY > document.getElementsByTagName("section")[0].offsetHeight + document.getElementsByTagName("section")[1].offsetHeight + document.getElementsByTagName("section")[2].offsetHeight/2) {
+			document.getElementsByTagName("section")[3].className = "active";
+	} else if(window.scrollY > document.getElementsByTagName("section")[0].offsetHeight + document.getElementsByTagName("section")[1].offsetHeight/2) {
+			document.getElementsByTagName("section")[2].className = "active";
+	} else if(window.scrollY > document.getElementsByTagName("section")[0].offsetHeight/2) {
+		document.getElementsByTagName("section")[1].className = "active";
+	}
+	
 });
